48e7b23f54d128425cd0fc88128658eb1d81a237
sinare0019 <sina.rezaei.pr1@gmail.com>
2023-01-20 22:08:09 +0330

    add Lambda to delegate

---
+++ DelegateLambda/DelegateLambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLambda
+{
+    delegate int MyDel(int number);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDel del;
+            del = (int number) => { return number + 15; };
+            int y = del(int.Parse(Console.ReadLine()));
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
